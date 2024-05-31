0db5e914adc29805e2db8f0e9f4ae8b9fdf23729
Anthony <amach510@gmail.com>
2024-05-31 00:30:37 -0700

    updated CustomActions.js

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -3,3 +3,85 @@ import { useActionSheet } from '@expo/react-native-action-sheet';
 import * as ImagePicker from 'expo-image-picker';
 import * as Location from 'expo-location';
 import { uploadBytes, ref, getDownloadURL } from "firebase/storage";
+
+const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, id }) => {
+    const actionSheet = useActionSheet();
+    const onActionPress = () => {
+        const options = ['Choose From Library', 'Take Picture', 'Send Location', 'Cancel'];
+        const cancelButtonIndex = options.length - 1;
+        actionSheet.showActionSheetWithOptions(
+            {
+                options,
+                cancelButtonIndex
+            },
+            async (buttonIndex) => {
+                switch (buttonIndex) {
+                    case 0:
+                        pickImage();
+                        return;
+                    case 1:
+                        takePhoto();
+                        return;
+                    case 2:
+                        getLocation();
+                    default:
+                }
+            },
+        );
+    };
+
+    // Get Location
+    const getLocation = async () => {
+        let permissions = await Location.requestForegroundPermissionsAsync();
+        if (permissions?.granted) {
+            const location = await Location.getCurrentPositionAsync({});
+            if (location) {
+                onSend({
+                    location: {
+                        longitude: location.coords.longitude,
+                        latitude: location.coords.latitude,
+                    },
+                });
+            } else Alert.alert("Error occurred while fetching location");
+        } else Alert.alert("Permissions to accsess loaction haven't been granted.");
+    }
+
+    // Name uploaded image
+    const generateReference = (uri) => {
+        const timeStamp = (new Date()).getTime();
+        const imageName = uri.split("/")[uri.split("/").length - 1];
+        return `${id}-${timeStamp}-${imageName}`;
+    }
+
+    return (
+        <TouchableOpacity style={styles.container} onPress={onActionPress}>
+            <View style={[styles.wrapper, wrapperStyle]}>
+                <Text style={[styles.iconText, iconTextStyle]}>+</Text>
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      width: 26,
+      height: 26,
+      marginLeft: 10,
+      marginBottom: 10,
+    },
+    wrapper: {
+      borderRadius: 13,
+      borderColor: '#b2b2b2',
+      borderWidth: 2,
+      flex: 1,
+    },
+    iconText: {
+      color: '#b2b2b2',
+      fontWeight: 'bold',
+      fontSize: 10,
+      backgroundColor: 'transparent',
+      textAlign: 'center',
+    },
+});
+
+export default CustomActions;
